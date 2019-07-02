1690bb1de59f66aac37167942e349687d2347244
Ivan Ryabchenko <rikdev@yandex.ru>
2019-07-02 22:54:33 +0700

    Add class transponder

--- include/channels/detail/compatibility/type_traits.h
+++ include/channels/detail/compatibility/type_traits.h
@@ -1,9 +1,41 @@
 #pragma once
+#include "functional.h"
 #include <type_traits>
 
 namespace channels {
 namespace detail {
 namespace compatibility {
+
+#if __cpp_lib_is_invocable
+using std::is_invocable; // NOLINT
+using std::is_invocable_v; // NOLINT
+using std::invoke_result; // NOLINT
+using std::invoke_result_t; // NOLINT
+#else
+namespace type_traits_detail {
+
+template<typename, typename... Ts>
+struct is_invocable_impl : std::false_type {};
+
+template<typename... Ts>
+struct is_invocable_impl<decltype(invoke(std::declval<Ts>()...)), Ts...> : std::true_type {};
+
+} // namespace type_traits_detail
+
+template<typename F, typename... Args>
+struct is_invocable : type_traits_detail::is_invocable_impl<void, F, Args...> {};
+
+template<typename F, typename... Args>
+constexpr bool is_invocable_v = is_invocable<F, Args...>::value;
+
+template<typename F, typename... Args>
+struct invoke_result {
+	using type = decltype(invoke(std::declval<F>(), std::declval<Args>()...));
+};
+
+template<typename F, typename... Args>
+using invoke_result_t = typename invoke_result<F, Args...>::type;
+#endif
 
 // remove_cvref
 template<typename T>
